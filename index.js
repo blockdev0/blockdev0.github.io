52a977c608fe95fdd0db742c1ede57b1afc2356e
blockdev0 <39884072+blockdev0@users.noreply.github.com>
2018-09-17 21:16:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 let btnFunction = async () => {
+  
   $('#butt').attr({target: '_blank', 
                     href  : 'https://github.com/blockdev0/blockdev0.github.io/raw/master/YoutubePlaylistDownloader-setup.exe'});
 }
