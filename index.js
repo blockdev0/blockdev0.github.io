2b0fe76d844bc033a4722ad410c904a13010e8e5
blockdev0 <39884072+blockdev0@users.noreply.github.com>
2019-02-10 01:19:20 +0100

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-
-let btnFunction = async () => {
-  
-  $('#butt').attr({target: '_blank', 
-                    href  : 'https://github.com/blockdev0/blockdev0.github.io/raw/master/YoutubePlaylistDownloader-setup.exe'});
-}
